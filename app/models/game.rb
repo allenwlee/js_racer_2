106521c976d900d6748ea4b9b9064ebc5b34b0ff
stephenitis <apprentice@devbootcamp.com>
2013-04-29 16:17:59 -0700

    created migrations and models for the game and players

---
+++ app/models/game.rb
@@ -0,0 +1,8 @@
+class Game < ActiveRecord::Base
+  has_and_belongs_to_many :players
+  validates :only_two
+
+  def only_two
+    errors.add(:player_count, "You need two freaking players dude.") unless self.players.count == 2
+  end
+end
