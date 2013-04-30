4d69f7016423aa0c13b26dfc9242982dfc39e4b9
stephenitis <nguyenhstephen@gmail.com>
2013-04-29 20:54:00 -0700

    stephen fixed some game functionality

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -2,12 +2,11 @@
 post '/game/create' do
    p params
    @game = Game.create
-   p @game.id
+
    @player1 = Player.create(name: params[:name1], game_id: @game.id)
    @player2 = Player.create(name: params[:name2], game_id: @game.id)
    @game.players << @player1
    @game.players << @player2
-   p @player1
-   p @player2
-  erb :_board
+   @board_length = params[:board_length]
+  erb :_board, :layout => false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -7,68 +7,73 @@ $(document).ready(function() {
       url: '/game/create',
       data: $(this).serialize()
     }).done(function(data){
-      $(".login").append(data);
+      $("#game").html(data);
+      var board_length = $("#board").val();
+      for ( i = 1; i < board_length; i++){
+        console.log(i);
+        $("#player1_strip").append($("<td></td>"));
+        $("#player2_strip").append($("<td></td>"));
+        current1 = $("#player1_strip td:first-child").addClass("active");
+        current2 = $("#player2_strip td:first-child").addClass("active");
+      }
     });
   });
 
 
-    var KEYS = {
-      P: 80,
-      Q: 81
-    };
-
-    current1 = $("#player1_strip td:first-child").addClass("active");
-    current2 = $("#player2_strip td:first-child").addClass("active");
 
 
 
-    $(this).on('keyup', function(e) {
-      if (e.keyCode === KEYS.Q) {
-        $(current1).removeClass("active");
-        $(current1).next().addClass("active");
-        current1 = $(current1).next();
-        if (current1.index() === $("#player1_strip td:last-child").index()) {
-          current1.removeClass("active");
-          current2.removeClass("active");
-          $(".container").append("PLAYER 1 WINS");
+  var KEYS = {P: 80,Q: 81};
+  $(this).on('keyup', function(e) {
+    if (e.keyCode === KEYS.Q) {
+      $(current1).removeClass("active");
+      $(current1).next().addClass("active");
+      current1 = $(current1).next();
+      if (current1.index() === $("#player1_strip td:last-child").index()) {
+        current1.removeClass("active");
+        current2.removeClass("active");
+        var versus1 = $("#versus1").text();
+        $(".container").append(" " + versus1 + "WINS");
         // $(".container").append('<img id='jkai' src="GoodJobSon.png" />');
         current1 = $("#player1_strip td:first-child").addClass("active");
         current2 = $("#player2_strip td:first-child").addClass("active");
 
       }
-    };
+    }
   });
 
-    $(this).on('keyup', function(e) {
-      if (e.keyCode === KEYS.P) {
-        $(current2).removeClass("active");
-        $(current2).next().addClass("active");
-        current2 = $(current2).next();
-        if (current2.index() === $("#player1_strip td:last-child").index()) {
-          current2.removeClass("active");
-          current1  .removeClass("active");
-          $(".container").append("PLAYER 2 WINS");
-          current1 = $("#player1_strip td:first-child").addClass("active");
-          current2 = $("#player2_strip td:first-child").addClass("active");
+  $(this).on('keyup', function(e) {
+    if (e.keyCode === KEYS.P) {
+      $(current2).removeClass("active");
+      $(current2).next().addClass("active");
+      current2 = $(current2).next();
+      var versus2 = $("#versus2").text();
+      if (current2.index() === $("#player1_strip td:last-child").index()) {
+        current2.removeClass("active");
+        current1  .removeClass("active");
+        $(".container").append(" "+ versus2 + "PLAYER 2 WINS");
+        current1 = $("#player1_strip td:first-child").addClass("active");
+        current2 = $("#player2_strip td:first-child").addClass("active");
 
-        }
-      };
-    });
-
-
-
+      }
+    }
   });
 
 
 
-// keyCode: 80 = p
-// keyCode: 81 = q
+});
 
-  // $(this).on('keyup', function(e) {
 
-  //    if (e.keyCode === KEYS.P) {
 
-  //     $(current2).removeClass("active");
-  //     $(current2).next().addClass("active");
-  //     current2 = $(current2).next();
-  //   };
+keyCode: 80 = p
+keyCode: 81 = q
+
+$(this).on('keyup', function(e) {
+
+ if (e.keyCode === KEYS.P) {
+
+  $(current2).removeClass("active");
+  $(current2).next().addClass("active");
+  current2 = $(current2).next();
+};
+});
