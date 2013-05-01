a747cb9ee33aee9720c65305aa60a7477bd50330
stephenitis <nguyenhstephen@gmail.com>
2013-04-30 17:21:56 -0700

    created JSON object, parsed JSON object into JS objects

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,5 +1,7 @@
 
 post '/game/create' do
+   
+
    p params
    @game = Game.create
 
@@ -7,6 +9,11 @@ post '/game/create' do
    @player2 = Player.create(name: params[:name2], game_id: @game.id)
    @game.players << @player1
    @game.players << @player2
+  p @game
    @board_length = params[:board_length]
-  erb :_board, :layout => false
+
+  # erb :_board, :layout => false
+  {:game => @game, :player1 => @game.players.first, :player2 => @game.players.last}.to_json
 end
+
+
--- public/js/application.js
+++ public/js/application.js
@@ -7,10 +7,15 @@ $(document).ready(function() {
       url: '/game/create',
       data: $(this).serialize()
     }).done(function(data){
-      $("#game").html(data);
+      var json = $.parseJSON(data)
+      
+      parseGameData(json);
+      // $("#game").html(json);
+      $(".gameboard").show();
+      
       var board_length = $("#board").val();
       for ( i = 1; i < board_length; i++){
-        console.log(i);
+        
         $("#player1_strip").append($("<td></td>"));
         $("#player2_strip").append($("<td></td>"));
         current1 = $("#player1_strip td:first-child").addClass("active");
@@ -19,7 +24,36 @@ $(document).ready(function() {
     });
   });
 
-// functionPlayer = function()
+  var Game = function (player1, player2, created_at, id) {
+    this.player1 = player1,
+    this.player2 = player2,
+    this.created_at = created_at
+
+
+  }
+
+  function parseGameData(json) {
+    var player1, player2, created_at, id
+
+    var game = json['game'];
+    for (var obj in game) {
+      created_at = game[obj].created_at;
+      id = game[obj].id;
+    }
+    var p1 = json['player1']
+    for (var obj in p1) {
+      player1 = p1[obj].name;
+    }
+    var p2 = json['player2']
+    for (var obj in p2) {
+      player2 = p2[obj].name;
+    }
+
+    current_game = new Game (player1, player2, created_at, id)
+    console.log(current_game)
+  }
+  // function Player () { this.name = name }
+
 
   function finish_line (player) {
     // var versus1 = $("#versus1").text();
@@ -71,15 +105,15 @@ $(document).ready(function() {
 });
 
 
-keyCode: 80 = p
-keyCode: 81 = q
+// keyCode: 80 = p
+// keyCode: 81 = q
 
-$(this).on('keyup', function(e) {
+// $(this).on('keyup', function(e) {
 
- if (e.keyCode === KEYS.P) {
+//  if (e.keyCode === KEYS.P) {
 
-  $(current2).removeClass("active");
-  $(current2).next().addClass("active");
-  current2 = $(current2).next();
-};
-});
+//   $(current2).removeClass("active");
+//   $(current2).next().addClass("active");
+//   current2 = $(current2).next();
+// };
+// });
